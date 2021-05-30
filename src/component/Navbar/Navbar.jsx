60f6c7d6a43b333c0786b5463d3307b744e0b3d1
jhonatanlopez <jhonatanlopezgonzalez@hotmail.es>
2021-05-29 22:38:14 -0500

    navbar y drawer medio funcionales

---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Drawer from '../Drawer/Drawer';
+import '../../style/Navbar.scss';
+import '../../icons/icomoon/style.css'
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    title: {
+      flexGrow: 1,
+    },
+}));
+
+
+const Navbar = () => {
+    const classes = useStyles();
+
+  return (
+    <div className={"containergeneral"}>
+      <AppBar position="static">
+        <Toolbar className={"containernabar"}>
+            <Drawer id={"drawer"}/>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default Navbar
