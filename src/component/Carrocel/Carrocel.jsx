f67c6a7ccc561dde5d07e403121a662fcaff0607
jhonatanlopez <jhonatanlopezgonzalez@hotmail.es>
2021-05-31 21:30:11 -0500

    contruccion en bloques de los  el rojo y el verde

--- src/component/Carrocel/Carrocel.jsx
+++ src/component/Carrocel/Carrocel.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
+import '../../style/Carrocel.scss';
 
 const Carrocel = () => {
     return (
-        <div className={"container general"}>
+        <div className={"containergeneral"}>
            <div className="row">
-                <div className="col-md4">
-                    <h1>hola </h1>
+                <div className={"containercolor"}>
+                    <h1>mundo  de las motos </h1>
                 </div> 
-                <div className="col-md8">
-                    <h1>que mas parce </h1>
+                <div className="containercarrocel">
+                    {/* <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                            <img class="d-block w-100" src="..." alt="First slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="..." alt="Second slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="..." alt="Third slide">
+                            </div>
+                        </div>
+                    </div> */}
                 </div>
            </div>
             
